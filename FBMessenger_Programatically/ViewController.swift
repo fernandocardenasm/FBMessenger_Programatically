8dc1db63e0582905817be0e0c577afe310ce3898
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-07-21 11:27:07 +0200

    Add Class Base Cell

--- FBMessenger_Programatically/ViewController.swift
+++ FBMessenger_Programatically/ViewController.swift
@@ -8,14 +8,55 @@
 
 import UIKit
 
-class FriendsController: UICollectionViewController {
+
+class FriendsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         collectionView?.backgroundColor = UIColor.blueColor()
+        collectionView?.alwaysBounceVertical = true
+        collectionView?.registerClass(FriendCell.self, forCellWithReuseIdentifier: cellId)
     }
 
-
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath)
+        return cell
+    }
+    
+    //Requires to add UICollectionViewDelegateFlowLayout
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(view.frame.width, 100)
+    }
 }
 
+class FriendCell: BaseCell {
+    
+    
+    
+    override func setUpViews() {
+        backgroundColor = UIColor.redColor()
+    }
+    
+}
+
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpViews() {
+        backgroundColor = UIColor.redColor()
+    }
+}
